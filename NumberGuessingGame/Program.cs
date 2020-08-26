6ef24ff8663b21c4044a851ec0bcc388facfc518
Kevin Daly <dalykb34@gmail.com>
2020-08-25 20:20:54 -0500

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($" What is your favorite school subject?");
+
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower()) 
+            {
+                case "physical education":
+                    Console.WriteLine("Mine too!");
+                    break;
+                case "english":
+                    Console.WriteLine("That is no fun.");
+                    break;
+                case "science":
+                    Console.WriteLine("Let do an experiment!");
+                        break;
+                case "math":
+                    Console.WriteLine("I would rather have Pi.");
+                        break;
+                case "art":
+                    Console.WriteLine("Can you draw a picture of me?");
+                        break;
+                default:
+                    Console.WriteLine("That is a new one!");
+                    break;
+
+            }
+
+        }
+    }
+}
